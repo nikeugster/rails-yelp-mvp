3a20fefa25d8a4af83aec1a7c8bd6230a14b91dc
Nik Eugster <mail@nikeugster.ch>
2021-05-19 14:17:04 +0200

    created seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurants = [
+  { name: "Fong Li", address: "23 Abbey Road, China Town", phone_number: "020 7420 5856", category: "chinese"},
+  { name: "Luigis Pizza", address: "Kleiststrasse 8, Berlin", phone_number: "030 2165925", category: "italian"},
+  { name: "La Fratelli", address: "Langstrasse 3, Zürich", phone_number: "044 257 25 36", category: "italian"},
+  { name: "Mon Paris", address: "235 Rue Jean-Pierre, Paris", phone_number: "01 40 64 77 64", category: "french"},
+  { name: "Sushi King", address: "Albertweg 3, Berlin", phone_number: "030 4256842", category: "japanese"}
+]
+
+restaurants.each do |restaurant|
+  Restaurant.create(name: restaurant[:name], address: restaurant[:address], phone_number: restaurant[:phone_number], category: restaurant[:category])
+end
