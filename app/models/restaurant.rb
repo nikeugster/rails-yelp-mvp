c417d94a261690386316baf72a304ab72849a011
Nik Eugster <mail@nikeugster.ch>
2021-05-19 12:48:22 +0200

    added validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,5 @@
 class Restaurant < ApplicationRecord
+  validates_presence_of :name, :address, :category
+  validates_inclusion_of :category, in: ["chinese", "italian", "japanese", "french", "belgian"]
+  has_many :reviews, dependent: :destroy
 end
